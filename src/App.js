2d6c96f9c0354cd29e2130a1feb83325987275c3
esther615kim <75744588+esther615kim@users.noreply.github.com>
2021-09-27 04:39:05 +0900

    created buttongroup created buttongroup, checkebox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import React, { useState } from "react";
 import "./styles.css";
+import {
+  Checkbox,
+  Button,
+  Typography,
+  TextField,
+  AppBar,
+  ButtonGroup,
+  FormControlLabel
+} from "@material-ui/core";
+//import { CheckBox } from "@material-ui/icons";
+//import { SaveIcon } from "@mui/icons-material";
+
+function ClickCheckbox() {
+  const [checked, setChecked] = useState(true); //기본설정:true;
+
+  return (
+    <>
+      <FormControlLabel
+        label="Testing checkbox"
+        control={
+          <Checkbox
+            //disabled
+            color="primary"
+            checked={checked}
+            onChange={(e) => setChecked(e.target.checked)} //check 바꿔주기
+          />
+        }
+      />
+    </>
+  );
+}
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <TextField />
+      {/* 함수로도 된다잉!! */}
+      <ClickCheckbox />
+      <ButtonGroup size="large" variant="contained">
+        <Button>save</Button>
+        <Button>delete</Button>
+        <Button>like</Button>
+      </ButtonGroup>
     </div>
   );
 }
