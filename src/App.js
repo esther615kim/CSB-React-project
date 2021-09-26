b3c1e09242efc4a9197a645c9327444da43fdf4d
esther615kim <75744588+esther615kim@users.noreply.github.com>
2021-09-27 04:54:31 +0900

    feat:added styled button added styled button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./styles.css";
 import {
+  makeStyles, //스타일링용
   Checkbox,
   Button,
   Typography,
@@ -12,6 +13,31 @@ import {
 //import { CheckBox } from "@material-ui/icons";
 //import { SaveIcon } from "@mui/icons-material";
 
+//스타일링
+const useStyles = makeStyles({
+  btn: {
+    // background: "linear-gradient(45deg, #333,#999)",
+    background: "linear-gradient(45deg, #FE6B8b,#ff8353)",
+    border: 0,
+    borderRadius: 5,
+    color: "white",
+    margin: 5,
+    padding: "10px 30px"
+  },
+  btnSmall: {
+    background: "linear-gradient(45deg, #333,#999)",
+    border: 0,
+    borderRadius: 5,
+    color: "white",
+    margin: 5,
+    padding: "10px 15px"
+  }
+});
+
+function StyledButton() {
+  const classes = useStyles();
+  return <Button className={classes.btn}>Styled Button</Button>;
+}
 function ClickCheckbox() {
   const [checked, setChecked] = useState(true); //기본설정:true;
 
@@ -35,14 +61,24 @@ function ClickCheckbox() {
 export default function App() {
   return (
     <div className="App">
-      <TextField />
-      {/* 함수로도 된다잉!! */}
+      {/* 날짜입력 type="date" 시간입력 type="time"*/}
+      <TextField type="date" mt={3} variant="outlined" />
+      {/* 이메일 type="email"
+      <TextField
+        label="your email"
+        type="email"
+        placeholder="test@test.com"
+        mt={3}
+      /> */}
+      {/* <TextField type="time" mt={3} variant="outlined" /> */}
+      {/* 함수를 컴포넌트로 입력할 수 있음!! */}
       <ClickCheckbox />
       <ButtonGroup size="large" variant="contained">
         <Button>save</Button>
         <Button>delete</Button>
         <Button>like</Button>
       </ButtonGroup>
+      <StyledButton />
     </div>
   );
 }
